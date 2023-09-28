5fd71c9ee6f649045e34b7ac471bba2cf4f58b9a
Tara <kislio01@gmail.com>
2023-09-27 22:11:29 -0400

    actually can submit new phones

--- src/lib/components/phonology/new-phone-wrapper.svelte
+++ src/lib/components/phonology/new-phone-wrapper.svelte
@@ -3,16 +3,37 @@
 
   import type { DescriptivePhone } from '$lib/types/phonology-types';
 
+  import { selectedLanguage } from '$lib/stores/language-store';
+
+  import { POST } from '$lib/utils/api-service';
+
   import PhoneCell from './phone-cell.svelte';
-  import TextInput from '../common/text-input.svelte';
   import PhoneCard from './phone-card.svelte';
+  import { invalidateAll } from '$app/navigation';
+  import Button from '../common/button.svelte';
 
   export let token: string | undefined = undefined;
   export let consonants: DescriptivePhone[];
 
+  let consonantMode = true;
+
   const { form, data, addField, unsetField } = createForm({
     initialValues: {
       phones: [] as string[]
+    },
+    onSubmit: async (values) => {
+      await POST('/phonology', {
+        token,
+        body: values.phones.map((phone) => ({
+          base_phone: phone,
+          language_id: $selectedLanguage,
+          vowel: !consonantMode
+        }))
+      });
+    },
+    onSuccess: async (response, { reset }) => {
+      await invalidateAll();
+      reset();
     }
   });
 
@@ -22,34 +43,33 @@
     {} as Record<string, number>
   );
 
-  $: {
-    console.log(selectedPhonesLookup);
-  }
-
   const removeCallback = (index: number) => unsetField(`phones.${index}`);
 </script>
 
-<form use:form class="flex gap-10">
-  <div>
-    <table>
-      <tbody>
-        <tr>
-          {#each consonants as { base_phone }}
-            <PhoneCell
-              {base_phone}
-              already_used={false}
-              addCallback={() => addField('phones', base_phone)}
-              {selectedPhonesLookup}
-              removeCallback={() => {}}
-            />
-          {/each}
-        </tr>
-      </tbody>
-    </table>
+<form use:form>
+  <div class="flex gap-10">
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            {#each consonants as { base_phone }}
+              <PhoneCell
+                {base_phone}
+                already_used={false}
+                addCallback={() => addField('phones', base_phone)}
+                {selectedPhonesLookup}
+                {removeCallback}
+              />
+            {/each}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="flex flex-wrap gap-5">
+      {#each selectedPhones as phone, i}
+        <PhoneCard base_phone={phone} index={i} removeCallback={() => removeCallback(i)} />
+      {/each}
+    </div>
   </div>
-  <div>
-    {#each selectedPhones as phone, i}
-      <PhoneCard base_phone={phone} index={i} removeCallback={() => removeCallback(i)} />
-    {/each}
-  </div>
+  <Button style="action" type="submit">Save New</Button>
 </form>
--- src/lib/components/phonology/phone-cell.svelte
+++ src/lib/components/phonology/phone-cell.svelte
@@ -2,28 +2,32 @@
   export let base_phone: string;
   export let already_used: boolean;
   export let addCallback: () => void;
-  export let removeCallback: () => void;
+  export let removeCallback: (index: number) => void;
   export let selectedPhonesLookup: Record<string, number>;
-
-  $: callback = () => {
-    addCallback();
-  };
 
   $: index = selectedPhonesLookup[base_phone];
 
   $: selected = index === 0 ? true : !!index;
 
-  const getStyles = () => {
-    const styles = [];
-    if (already_used) {
-      styles.push('brightness-75');
-    } else if (selected) {
-      styles.push('bg-action-primary');
-    }
-    return styles.join(' ');
+  $: callback = () => {
+    if (already_used) return () => {};
+    if (selected) return removeCallback(index);
+    return addCallback();
   };
+
+  let styles = '';
+
+  $: {
+    const styleArray = [];
+    if (already_used) {
+      styleArray.push('brightness-75');
+    } else if (selected) {
+      styleArray.push('bg-action-primary');
+    }
+    styles = styleArray.join(' ');
+  }
 </script>
 
-<td class={getStyles()} on:click={callback}>
+<td class={styles} on:click={callback}>
   {base_phone}
 </td>
